e938a8c39e6e9b5adb6756a5bafc8c2a98d687d2
Chris Zheng <chris@statstrade.io>
2025-11-26 19:46:18 +1100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xyz.zcaudate/foundation-base "4.0.12"
+(defproject xyz.zcaudate/foundation-base "4.1.0"
   :description "base libraries for foundation"
   :url "https://www.github.com/zcaudate-xyz/foundation-base"
   :license  {:name "MIT License"
