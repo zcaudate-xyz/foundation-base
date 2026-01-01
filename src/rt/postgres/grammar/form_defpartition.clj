239e3d2e32393e01e8ddfb73af6fa9f1aae63e6e
Chris Zheng <zcaudate@users.noreply.github.com>
2026-01-01 14:58:58 +1100

    adding partitions

--- src/rt/postgres/grammar/form_defpartition.clj
+++ src/rt/postgres/grammar/form_defpartition.clj
@@ -1,6 +1,9 @@
 (ns rt.postgres.grammar.form-defpartition
   (:require [rt.postgres.grammar.common :as common]
             [std.lang.base.grammar-spec :as grammar-spec]
+            [std.lang.base.emit-preprocess :as preprocess]
+            [std.lang.base.library-snapshot :as snap]
+            [std.lang.base.book :as book]
             [std.string :as str]
             [std.lib :as h]))
 
@@ -8,34 +11,37 @@
   "constructs partition name"
   {:added "4.0"}
   [base val stack]
-  (let [parts (cons base (cons val stack))]
+  (let [parts (concat [base] (reverse stack) [val])]
     (clojure.string/join "_" (map h/strn parts))))
 
 (defn pg-partition-def
   "recursive definition for partition"
   {:added "4.0"}
   [parent-sym base-name current-spec remaining-specs stack]
-  (let [{:keys [use in default]} current-spec
+  (let [{:keys [use in default schema]} current-spec
         next-spec (first remaining-specs)
         next-col  (when-let [u (:use next-spec)] (clojure.string/replace (name u) "-" "_"))
-        parent-ns (namespace parent-sym)
-        [schema-prefix table-name] (if parent-ns
-                                     [nil base-name]
-                                     (let [parts (clojure.string/split (name parent-sym) #"\.")]
-                                       (if (> (count parts) 1)
-                                         [(first parts) (last parts)]
-                                         [nil base-name])))
         
-        schema (or parent-ns schema-prefix)
-        full-parent (if schema
-                      (list '. #{schema} #{table-name})
-                      #{table-name})]
+        ;; Parent info
+        p-ns (namespace parent-sym)
+        [p-sch p-tab] (if p-ns
+                        [p-ns (name parent-sym)]
+                        (let [parts (clojure.string/split (name parent-sym) #"\.")]
+                          (if (> (count parts) 1)
+                            [(first parts) (last parts)]
+                            [nil (name parent-sym)])))
+        
+        full-parent (if p-sch
+                      (list '. #{p-sch} #{p-tab})
+                      #{p-tab})
+        
+        ;; Current level schema
+        curr-sch (or schema p-sch)]
     
     (if default
-      (let [new-name (str table-name "_default")
-            full-new (if schema
-                       (list '. #{schema} #{new-name})
-                       #{new-name})]
+      (let [new-name (pg-partition-name base-name "default" stack)
+            new-sym  (if curr-sch (symbol curr-sch new-name) (symbol new-name))
+            full-new (if curr-sch (list '. #{curr-sch} #{new-name}) #{new-name})]
         (list
          (vec (concat [:create-table :if-not-exists full-new
                        :partition-of full-parent :default]
@@ -45,13 +51,9 @@
                       #_[\;]))))
       
       (mapcat (fn [val]
-                (let [new-name (pg-partition-name table-name val stack)
-                      new-sym  (cond parent-ns (symbol parent-ns new-name)
-                                     schema-prefix (symbol (str schema-prefix "." new-name))
-                                     :else (symbol new-name))
-                      full-new (if schema
-                                 (list '. #{schema} #{new-name})
-                                 #{new-name})]
+                (let [new-name (pg-partition-name base-name val stack)
+                      new-sym  (if curr-sch (symbol curr-sch new-name) (symbol new-name))
+                      full-new (if curr-sch (list '. #{curr-sch} #{new-name}) #{new-name})]
                   
                   (concat
                    [(vec (concat [:create-table :if-not-exists full-new
@@ -62,16 +64,36 @@
                                    [])
                                  #_[\;]))]
                    (if (seq remaining-specs)
-                     (pg-partition-def new-sym table-name next-spec (rest remaining-specs) (cons val stack))
+                     (pg-partition-def new-sym base-name next-spec (rest remaining-specs) (cons val stack))
                      nil))))
               in))))
 
 (defn pg-defpartition
   "defpartition block"
   {:added "4.0"}
-  [[_ sym doc? attr? [parent] specs :as form]]
-  (let [[{:keys [doc] :as mdefn}
-         [_ sym [parent] specs]] (grammar-spec/format-defn form)
-        base-name (name parent)
-        statements (pg-partition-def parent base-name (first specs) (rest specs) [])]
-     (apply list 'do statements)))
+  ([form]
+   (let [mopts (preprocess/macro-opts)
+         [mdefn [_ sym [parent] specs]] (grammar-spec/format-defn form)
+         
+         parent-sym (if (vector? parent) (first parent) parent)
+         
+         ;; Try to find schema from parent entry
+         p-en  (if (symbol? parent-sym)
+                 (let [snapshot (:snapshot mopts)
+                       lang     (:lang mopts)
+                       book     (if snapshot (snap/get-book snapshot lang))
+                       module-id (or (namespace parent-sym) (:module mopts))
+                       entry-id  (name parent-sym)]
+                   (if book
+                     (book/get-base-entry book (symbol module-id) (symbol entry-id) :code))))
+         
+         parent-schema (or (:static/schema p-en) (namespace parent-sym))
+         base-name     (name parent-sym)
+         
+         ;; Update parent with found schema for SQL generation
+         parent-sql-sym (if parent-schema
+                          (symbol parent-schema base-name)
+                          (symbol base-name))
+         
+         statements (pg-partition-def parent-sql-sym base-name (first specs) (rest specs) [])]
+     (apply list 'do statements))))
--- test/rt/postgres/grammar/form_defpartition_test.clj
+++ test/rt/postgres/grammar/form_defpartition_test.clj
@@ -9,18 +9,32 @@
   ^:hidden
   
   (form-defpartition/pg-partition-name "table" "val" ["stack"])
-  => "table_val_stack")
+  => "table_stack_val")
 
 ^{:refer rt.postgres.grammar.form-defpartition/pg-partition-def :added "4.1"}
 (fact "recursive definition for partition"
   ^:hidden
   
   (form-defpartition/pg-partition-def 'parent "base" {:use :col :in ["a"]} [] [])
-  => '([:create-table :if-not-exists #{"base_a"} :partition-of #{"base"} :for :values :in (quote ("a"))]))
+  => '([:create-table :if-not-exists #{"base_a"} :partition-of #{"parent"} :for :values :in (quote ("a"))]))
 
 ^{:refer rt.postgres.grammar.form-defpartition/pg-defpartition :added "4.1"}
 (fact "defpartition block"
   ^:hidden
 
   (form-defpartition/pg-defpartition '(defpartition part [parent] [{:use :col :in ["a"]}]))
-  => '(do [:create-table :if-not-exists #{"parent_a"} :partition-of #{"parent"} :for :values :in (quote ("a"))]))
+  => '(do [:create-table :if-not-exists #{"parent_a"} :partition-of #{"parent"} :for :values :in (quote ("a"))])
+
+  (fact "defpartition handles nested partitions and schemas"
+    (form-defpartition/pg-defpartition 
+     '(defpartition RevPartitionBase
+        [szn_type/Rev]
+        [{:use :class :schema "szn_type" :in ["Default"]}
+         {:use :class-table :schema "szn_type_rev" :in ["Feed"]}]))
+    => '(do [:create-table :if-not-exists (. #{"szn_type"} #{"Rev_Default"})
+             :partition-of (. #{"szn_type"} #{"Rev"})
+             :for :values :in (quote ("Default"))
+             :partition-by :list (quote (class_table))]
+            [:create-table :if-not-exists (. #{"szn_type_rev"} #{"Rev_Default_Feed"})
+             :partition-of (. #{"szn_type"} #{"Rev_Default"})
+             :for :values :in (quote ("Feed"))])))
