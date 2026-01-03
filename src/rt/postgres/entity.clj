ea7d6eefdac4497d6b120f42e4c3670316e29e56
Chris Zheng <zcaudate@users.noreply.github.com>
2026-01-03 17:42:27 +1100

    refactoring files

--- src/rt/postgres/entity.clj
+++ src/rt/postgres/entity.clj
@@ -1,277 +1,21 @@
-(ns rt.postgres.entity-types
+(ns rt.postgres.entity
   (:require [std.lib :as h]
             [std.lang :as l]
             [std.string :as str]
             [rt.postgres :as pg]
-            [std.lang.base.grammar-spec :as grammar-spec]))
-
-(defonce +app+
-  (atom {}))
-
-(def ^:dynamic *ns-str* "-")
-
-(defn default-application
-  [& [module]]
-  (first (:application
-          (:static (h/suppress
-                    (l/rt:module
-                     (l/rt (or module
-                               (h/ns-sym))
-                           :postgres)))))))
-
-(defn default-ns-str
-  [& [application]]
-  (get @+app+ (or application
-                  (default-application (h/ns-sym)))))
-
-(defn init-default-ns-str
-  [& [application ns-str]]
-  (let [ns-str (or ns-str (name (h/ns-sym)))
-        application (or application (default-application (h/ns-sym)))]
-    (when application 
-      (swap! +app+ assoc application ns-str))))
-
-
-
-
-;;
-;; Default Types
-;;
-
-(defn type-id-v1
-  []
-  {:type :uuid :primary "default"
-   :sql {:default '(rt.postgres/uuid-generate-v1)}})
-
-(defn type-id-v4
-  []
-  {:type :uuid :primary "default"
-   :sql {:default '(rt.postgres/uuid-generate-v4)}})
-
-(defn type-id-text
-  [ns-str]
-  {:type :citext :primary "default"
-   :sql {:process [[(symbol ns-str "as-upper-formatted")]
-                   [(symbol ns-str "as-upper-limit-length") 100]]}})
-
-(defn type-name
-  [ns-str]
-  {:type :citext :required true :scope :-/info
-   :sql  {:process [[(symbol ns-str "as-upper-formatted")]
-                   [(symbol ns-str "as-upper-limit-length") 36]]
-          :unique ["name"]}})
-
-(defn type-image
-  [ns-str]
-  {:type :image
-   :sql  {:process [[(symbol ns-str "as-jsonb")]]
-          :default "{}"}
-   :profiles {:web {:type "image"}}})
-
-(defn type-color
-  [ns-str]
-  {:type :citext :required true :scope :-/info
-   :sql {:default    (list (symbol ns-str "color-rand"))
-         :constraint (list (symbol ns-str "color-check") #{"color"})}
-   :profile {:web {:edit #{:create :modify}
-                   :type "color"}}})
-
-(defn type-tags
-  [ns-str]
-  {:type :array :scope :-/info
-   :sql {:process [[(symbol ns-str "as-jsonb-array")]]
-         :default "[]"}
-   :profile {:web {:edit #{:create :modify}
-                   :type "chip"}}})
-
-(defn type-log
-  [ns-str]
-  {:type :array :required true
-   :scope :-/detail
-   :sql  {:process [[(symbol ns-str "as-jsonb-array")]]
-          :default "[]"}
-   :map  {:status  {:type :text}
-          :message {:type :text}
-          :error   {:type :text}}})
-
-(defn type-boolean
-  [& [default]]
-  {:type :boolean :required true
-   :sql {:default (not (not default))}})
-
-(defn type-class-table
-  [ns-str]
-  {:type :enum :scope :-/system
-   :enum {:ns (symbol ns-str "EnumClassTableType")}})
-
-(defn type-class-context
-  [ns-str]
-  {:type :enum :scope :-/system
-   :enum {:ns  (symbol ns-str "EnumClassContextType")}})
-
-(defn type-ref
-  [ns-str table-str]
-  {:type :ref :required true
-   :ref {:ns (symbol ns-str table-str)}})
-
-(defn type-class-ref
-  []
-  {:type :uuid :required true})
-
-(defn type-class-key
-  [ns-str]
-  {:type :citext :required true
-   :sql {:process [[(symbol ns-str "as-upper-formatted")]
-                   [(symbol ns-str "as-upper-limit-length") 100]]}})
-
-
-(defn normalise-ref
-  [ptr]
-  (cond (h/pointer? ptr)
-        (symbol (name (:module ptr))
-                (name (:id ptr)))
-
-        (symbol? ptr) ptr
-
-        (var? ptr) (symbol (name (.getName (.ns ptr)))
-                           (name (.sym ptr)))))
-
-(defonce +fields+
-  (atom {}))
-
-(defn default-fields
-  [ns-str]
-  {:id            {:priority     0  :field    (type-id-v4)}
-   :class-table   {:priority     1  :field    (type-class-table ns-str)}
-   :class-context {:priority     2  :field    (type-class-context ns-str)}
-   :class-ref     {:priority     3  :field    (type-class-ref)}
-   :name          {:priority     5  :field    (type-name  ns-str)}
-   :icon          {:priority    10  :field    (type-image ns-str)}
-   :picture       {:priority    11  :field    (type-image ns-str)}
-   :background    {:priority    12  :field    (type-image ns-str)}
-   :color         {:priority    20  :field    (type-color ns-str)}
-   :tags          {:priority    25  :field    (type-tags  ns-str)}
-   :is-active     {:priority    30  :field    (type-boolean true)}
-   :is-public     {:priority    31  :field    (type-boolean true)}
-   :is-official   {:priority    80  :field    (type-boolean false)}
-   :is-onboarded  {:priority    81  :field    (type-boolean false)}
-   :log           {:priority   100  :field    (type-log   ns-str)}})
-
-;;
-;; Default Addons
-;;
-
-(defonce +addons+
-  (atom {}))
-
-(defn init-addons
-  [& [m application]]
-  (let [application   (or application (default-application (h/ns-sym)))
-        ns-str   (default-ns-str application)]
-    (when application 
-      (swap! +addons+ assoc application (merge m (default-fields ns-str))))))
-
-(defn get-addon
-  [key]
-  (assoc (get-in @+addons+ [(default-application (h/ns-sym))
-                            key])
-         :key key))
-
-(defn add-addon
-  [key field priority]
-  (let [application (default-application (h/ns-sym))]
-    (swap! +addons+ assoc-in [application key] {:field field
-                                                :priority priority})))
-
-(defn addons-remove
-  [key]
-  (swap! +addons+ update-in [(default-application (h/ns-sym))] dissoc key))
-
-
-;;
-;; Default Tracking
-;;
-
-(defn get-tracking
-  [track]
-  (case track
-    (:track/record :track/data)
-    {:name   "data"
-     :in     {:create {:time-created :time
-                       :time-updated :time
-                       :op-created :id
-                       :op-updated :id}
-              :modify {:time-updated :time
-                       :op-updated :id}}
-     :ignore #{:delete}}
-
-    :track/temp
-    {:name    "temp"
-     :in      {:create  {:time-created :time
-                         :time-updated :time}
-               :modify {:time-updated :time}}
-     :ignore  #{:delete}}
-    
-    :track/log
-    {:name    "log"
-     :in      {:create {:op-created :id :time-created :time}}
-     :disable #{:modify}
-     :ignore  #{:delete}}
-
-    :track/none {}))
-
-(defn get-tracking-columns
-  [track]
-  (case track
-    :track/record [:op-created   {:type :uuid}
-                   :op-updated   {:type :uuid}
-                   :time-created {:type :time}
-                   :time-updated {:type :time}
-                   :__deleted__  {:type :boolean
-                                  :scope :-/hidden
-                                  :sql {:default false}}]
-    
-    :track/data    [:op-created   {:type :uuid}
-                    :op-updated   {:type :uuid}
-                    :time-created {:type :time}
-                    :time-updated {:type :time}]
-    
-    :track/log     [:op-created   {:type :uuid}
-                    :time-created {:type :time}]
-
-    :track/temp    [:time-created {:type :time}
-                    :time-updated {:type :time}]
-
-    :track/none     []))
-
-(defn get-access
-  [access]
-  (if (not= access :none)
-    {:sb/rls true
-     :sb/access {:admin :all
-                 :auth  (case access
-                          (:access/hidden) :none
-                          (:access/system
-                           :access/auth
-                           :access/public)  :select)
-                 :anon  (case access
-                          (:access/hidden
-                           :access/system)  :none
-                          (:access/auth
-                           :access/public)  :select)}}))
-
-(defn fill-priority
-  [arr priority]
-  (let [curr (volatile! -1)]
-    (mapv (fn [m]
-            (if (map? m)
-              (assoc m :priority priority :priority-index (vswap! curr inc))
-              m))
-          arr)))
+            [std.lang.base.grammar-spec :as grammar-spec]
+            [rt.postgres.entity-util :as ut]))
 
 ;;
 ;; E
 ;;
+
+(def LinkSpec
+  {:entity   {:2d/log   #{:2d/base}
+              :1d/log   #{:1d/entry}
+              :1d/entry #{:1d/base}}
+   :addons   {:1d/entry #{:2d/base}
+              :0d/entry #{:2d/base :1d/base}}})
 
 (def ESpec
   {:id        #{:id/none :id/v4 :id/v1  :id/text map?}
@@ -318,6 +62,8 @@
       m)))
 
 
+
+
 ;;
 ;;  :2d/base :2d/log will always have  :class-context and :class-table 
 ;;  :0d/entry will also always have :class-context and :class-table
@@ -335,8 +81,8 @@
                (h/error "Need a :for keyword" {:input m}))
         [key ref] (if (vector? for)
                     for
-                    [(keyword (str/spear-case (name (normalise-ref for)))) for])
-        ref (normalise-ref ref)
+                    [(keyword (str/spear-case (name (ut/normalise-ref for)))) for])
+        ref (ut/normalise-ref ref)
         unique (or unique
                    (if (not= "log" (name class))
                      [(str/snake-case (name key))]))
@@ -361,7 +107,7 @@
                  {:key key
                   :field 
                   {:type :ref :required true
-                   :ref {:ns (normalise-ref ref)}}})
+                   :ref {:ns (ut/normalise-ref ref)}}})
         addon  (cond (vector? v)
                      (let [[key ref priority custom] v]
                        (h/merge-nested
@@ -370,10 +116,10 @@
                         custom))
 
                      (keyword? v)
-                     (get-addon v)
+                     (ut/get-addon v)
                      
                      (map? v)
-                     (let [addon  (get-addon (:key v))]
+                     (let [addon  (ut/get-addon (:key v))]
                        (merge addon
                               (dissoc v :ref)
                               (if (:ref v)
@@ -424,7 +170,7 @@
 
 (defn E-class-columns
   [{:keys [symname class entity ns-str]
-    :or {ns-str *ns-str*
+    :or {ns-str ut/*ns-str*
          class   :none
          symname "Global"}
     :as m}]
@@ -436,13 +182,13 @@
                             :2d/log} class)
                        (E-entity-class-fields m))
         base (cond-> (merge
-                      {:class-table   (assoc (type-class-table ns-str)   :priority 2)}
+                      {:class-table   (assoc (ut/type-class-table ns-str)   :priority 2)}
                       (if (not= class :1d/log)
-                        {:class-context (assoc (type-class-context ns-str) :priority 3)}))
+                        {:class-context (assoc (ut/type-class-context ns-str) :priority 3)}))
                :then (h/merge-nested entry-fields)
                (#{:1d/base
                   :2d/base} class)  (assoc :class-ref
-                                           (assoc (type-class-ref)
+                                           (assoc (ut/type-class-ref)
                                                   :priority 4
                                                   :sql {:unique ["class"]})))]
     (case (namespace class)
@@ -464,6 +210,7 @@
                                :class-context {:sql {:unique ["class"]}}}))
       {})))
 
+
 (defn E-class-merge
   [m id-cols track-cols class-cols addon-cols]
   (let [{:keys [class columns]} m
@@ -475,7 +222,7 @@
                                                  (:unique sql)))
                                          (mapcat h/seqify)
                                          (set)
-                                                  (vec))
+                                         (vec))
                         nil)]
     (cond-> final-cols 
       class-uniques (h/merge-nested
@@ -496,14 +243,14 @@
         [id track] [(or id id-in) (or track track-in)]
         id   (cond (map? id) id
                    :else (case id
-                           :id/v1   (type-id-v1)
-                           :id/v4   (type-id-v4)
-                           :id/text (type-id-text ns-str)
+                           :id/v1   (ut/type-id-v1)
+                           :id/v4   (ut/type-id-v4)
+                           :id/text (ut/type-id-text ns-str)
                            :id/none nil))
         
-        access-val   (get-access access)
-        track-val    (get-tracking track)
-        track-cols   (apply hash-map (fill-priority (get-tracking-columns track) 200))
+        access-val   (ut/get-access access)
+        track-val    (ut/get-tracking track)
+        track-cols   (apply hash-map (ut/fill-priority (ut/get-tracking-columns track) 200))
         
         id-cols      (if id {:id (assoc id :priority 0)})
         class-cols   (E-class-columns m)
@@ -524,6 +271,8 @@
                            (or (:priority-index v) 0)]))
                vec)}))
 
+
+
 (defn E
   [{:keys [id class entity track access raw columns ns-str application]
     :as m}]
@@ -531,8 +280,8 @@
                   :class   :none
                   :access  :access/auth
                   :columns []
-                  :ns-str  (or (default-ns-str application)
-                               *ns-str* 
+                  :ns-str  (or (ut/default-ns-str application)
+                               ut/*ns-str* 
                                (h/error "No ns-str found." {:ns (h/ns-sym)}))}
                  m)
         _ (E-check-input m)]
---
+++ src/rt/postgres/entity_util.clj
@@ -0,0 +1,270 @@
+(ns rt.postgres.entity-util
+  (:require [std.lib :as h]
+            [std.lang :as l]
+            [std.string :as str]
+            [rt.postgres :as pg]
+            [std.lang.base.grammar-spec :as grammar-spec]))
+
+(defonce +app+
+  (atom {}))
+
+(def ^:dynamic *ns-str* "-")
+
+(defn default-application
+  [& [module]]
+  (first (:application
+          (:static (h/suppress
+                    (l/rt:module
+                     (l/rt (or module
+                               (h/ns-sym))
+                           :postgres)))))))
+
+(defn default-ns-str
+  [& [application]]
+  (get @+app+ (or application
+                  (default-application (h/ns-sym)))))
+
+(defn init-default-ns-str
+  [& [application ns-str]]
+  (let [ns-str (or ns-str (name (h/ns-sym)))
+        application (or application (default-application (h/ns-sym)))]
+    (when application 
+      (swap! +app+ assoc application ns-str))))
+
+
+
+
+;;
+;; Default Types
+;;
+
+(defn type-id-v1
+  []
+  {:type :uuid :primary "default"
+   :sql {:default '(rt.postgres/uuid-generate-v1)}})
+
+(defn type-id-v4
+  []
+  {:type :uuid :primary "default"
+   :sql {:default '(rt.postgres/uuid-generate-v4)}})
+
+(defn type-id-text
+  [ns-str]
+  {:type :citext :primary "default"
+   :sql {:process [[(symbol ns-str "as-upper-formatted")]
+                   [(symbol ns-str "as-upper-limit-length") 100]]}})
+
+(defn type-name
+  [ns-str]
+  {:type :citext :required true :scope :-/info
+   :sql  {:process [[(symbol ns-str "as-upper-formatted")]
+                   [(symbol ns-str "as-upper-limit-length") 36]]
+          :unique ["name"]}})
+
+(defn type-image
+  [ns-str]
+  {:type :image
+   :sql  {:process [[(symbol ns-str "as-jsonb")]]
+          :default "{}"}
+   :profiles {:web {:type "image"}}})
+
+(defn type-color
+  [ns-str]
+  {:type :citext :required true :scope :-/info
+   :sql {:default    (list (symbol ns-str "color-rand"))
+         :constraint (list (symbol ns-str "color-check") #{"color"})}
+   :profile {:web {:edit #{:create :modify}
+                   :type "color"}}})
+
+(defn type-tags
+  [ns-str]
+  {:type :array :scope :-/info
+   :sql {:process [[(symbol ns-str "as-jsonb-array")]]
+         :default "[]"}
+   :profile {:web {:edit #{:create :modify}
+                   :type "chip"}}})
+
+(defn type-log
+  [ns-str]
+  {:type :array :required true
+   :scope :-/detail
+   :sql  {:process [[(symbol ns-str "as-jsonb-array")]]
+          :default "[]"}
+   :map  {:status  {:type :text}
+          :message {:type :text}
+          :error   {:type :text}}})
+
+(defn type-boolean
+  [& [default]]
+  {:type :boolean :required true
+   :sql {:default (not (not default))}})
+
+(defn type-class-table
+  [ns-str]
+  {:type :enum :scope :-/system
+   :enum {:ns (symbol ns-str "EnumClassTableType")}})
+
+(defn type-class-context
+  [ns-str]
+  {:type :enum :scope :-/system
+   :enum {:ns  (symbol ns-str "EnumClassContextType")}})
+
+(defn type-ref
+  [ns-str table-str]
+  {:type :ref :required true
+   :ref {:ns (symbol ns-str table-str)}})
+
+(defn type-class-ref
+  []
+  {:type :uuid :required true})
+
+(defn type-class-key
+  [ns-str]
+  {:type :citext :required true
+   :sql {:process [[(symbol ns-str "as-upper-formatted")]
+                   [(symbol ns-str "as-upper-limit-length") 100]]}})
+
+
+(defn normalise-ref
+  [ptr]
+  (cond (h/pointer? ptr)
+        (symbol (name (:module ptr))
+                (name (:id ptr)))
+
+        (symbol? ptr) ptr
+
+        (var? ptr) (symbol (name (.getName (.ns ptr)))
+                           (name (.sym ptr)))))
+
+(defonce +fields+
+  (atom {}))
+
+(defn default-fields
+  [ns-str]
+  {:id            {:priority     0  :field    (type-id-v4)}
+   :class-table   {:priority     1  :field    (type-class-table ns-str)}
+   :class-context {:priority     2  :field    (type-class-context ns-str)}
+   :class-ref     {:priority     3  :field    (type-class-ref)}
+   :name          {:priority     5  :field    (type-name  ns-str)}
+   :icon          {:priority    10  :field    (type-image ns-str)}
+   :picture       {:priority    11  :field    (type-image ns-str)}
+   :background    {:priority    12  :field    (type-image ns-str)}
+   :color         {:priority    20  :field    (type-color ns-str)}
+   :tags          {:priority    25  :field    (type-tags  ns-str)}
+   :is-active     {:priority    30  :field    (type-boolean true)}
+   :is-public     {:priority    31  :field    (type-boolean true)}
+   :is-official   {:priority    80  :field    (type-boolean false)}
+   :is-onboarded  {:priority    81  :field    (type-boolean false)}
+   :log           {:priority   100  :field    (type-log   ns-str)}})
+
+;;
+;; Default Addons
+;;
+
+(defonce +addons+
+  (atom {}))
+
+(defn init-addons
+  [& [m application]]
+  (let [application   (or application (default-application (h/ns-sym)))
+        ns-str   (default-ns-str application)]
+    (when application 
+      (swap! +addons+ assoc application (merge m (default-fields ns-str))))))
+
+(defn get-addon
+  [key]
+  (assoc (get-in @+addons+ [(default-application (h/ns-sym))
+                            key])
+         :key key))
+
+(defn add-addon
+  [key field priority]
+  (let [application (default-application (h/ns-sym))]
+    (swap! +addons+ assoc-in [application key] {:field field
+                                                :priority priority})))
+
+(defn addons-remove
+  [key]
+  (swap! +addons+ update-in [(default-application (h/ns-sym))] dissoc key))
+
+
+;;
+;; Default Tracking
+;;
+
+(defn get-tracking
+  [track]
+  (case track
+    (:track/record :track/data)
+    {:name   "data"
+     :in     {:create {:time-created :time
+                       :time-updated :time
+                       :op-created :id
+                       :op-updated :id}
+              :modify {:time-updated :time
+                       :op-updated :id}}
+     :ignore #{:delete}}
+
+    :track/temp
+    {:name    "temp"
+     :in      {:create  {:time-created :time
+                         :time-updated :time}
+               :modify {:time-updated :time}}
+     :ignore  #{:delete}}
+    
+    :track/log
+    {:name    "log"
+     :in      {:create {:op-created :id :time-created :time}}
+     :disable #{:modify}
+     :ignore  #{:delete}}
+
+    :track/none {}))
+
+(defn get-tracking-columns
+  [track]
+  (case track
+    :track/record [:op-created   {:type :uuid}
+                   :op-updated   {:type :uuid}
+                   :time-created {:type :time}
+                   :time-updated {:type :time}
+                   :__deleted__  {:type :boolean
+                                  :scope :-/hidden
+                                  :sql {:default false}}]
+    
+    :track/data    [:op-created   {:type :uuid}
+                    :op-updated   {:type :uuid}
+                    :time-created {:type :time}
+                    :time-updated {:type :time}]
+    
+    :track/log     [:op-created   {:type :uuid}
+                    :time-created {:type :time}]
+
+    :track/temp    [:time-created {:type :time}
+                    :time-updated {:type :time}]
+
+    :track/none     []))
+
+(defn get-access
+  [access]
+  (if (not= access :none)
+    {:sb/rls true
+     :sb/access {:admin :all
+                 :auth  (case access
+                          (:access/hidden) :none
+                          (:access/system
+                           :access/auth
+                           :access/public)  :select)
+                 :anon  (case access
+                          (:access/hidden
+                           :access/system)  :none
+                          (:access/auth
+                           :access/public)  :select)}}))
+
+(defn fill-priority
+  [arr priority]
+  (let [curr (volatile! -1)]
+    (mapv (fn [m]
+            (if (map? m)
+              (assoc m :priority priority :priority-index (vswap! curr inc))
+              m))
+          arr)))
