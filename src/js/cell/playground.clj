c0e1e0c5966a52f1ac0cb739200faad3e9f2faa1
Chris Zheng <chris@statstrade.io>
2026-03-28 11:34:50 +1100

    REVERT SUSPECT FILES

--- src/js/cell/playground.clj
+++ src/js/cell/playground.clj
@@ -17,39 +17,10 @@
               [:head
                [:meta {:http-equiv "Content-Security-Policy"
                        :content (clojure.string/join ";"
-                                                     ["default-src * 'unsafe-eval'"
-                                                      "connect-src * 'unsafe-eval'"
-                                                      "script-src  'self' 'unsafe-eval' 'unsafe-inline"
-                                                      "worker-src  blob: data: 'self' 'unsafe-eval' 'unsafe-inline"])}]]]))
-
-(defn- page-html
-  "renders a page scaffold for browser e2e tests"
-  [{:keys [title head body scripts]
-    :or {title "js.cell playground"
-         body [:div {:id "playground"}]}}]
-  (let [csp (clojure.string/join ";"
-                                 ["default-src * 'unsafe-eval'"
-                                  "connect-src * 'unsafe-eval'"
-                                  "script-src  'self' 'unsafe-eval' 'unsafe-inline'"
-                                  "worker-src  blob: data: 'self' 'unsafe-eval' 'unsafe-inline'"])
-        body (if (string? body)
-               body
-               (html/html body))
-        head (cond (nil? head) ""
-                   (string? head) head
-                   :else (html/html head))
-        scripts (apply str (map (fn [src]
-                                  (str "<script src=\"" src "\"></script>"))
-                                scripts))]
-    (str "<!doctype html>"
-         "<html><head>"
-         "<meta http-equiv=\"Content-Security-Policy\" content=\"" csp "\">"
-         "<title>" title "</title>"
-         head
-         "</head><body>"
-         body
-         scripts
-         "</body></html>")))
+                                          ["default-src * 'unsafe-eval'"
+                                           "connect-src * 'unsafe-eval'"
+                                           "script-src  'self' 'unsafe-eval' 'unsafe-inline"
+                                           "worker-src  data: 'self' 'unsafe-eval' 'unsafe-inline"])}]]]))
 
 (defn start-playground
   "starts the playground"
@@ -61,7 +32,7 @@
                     _    (spit (str root "/index.html") +index+)
                     port (network/port:check-available 0)
                     process (os/sh "http-server" "-p" (str port) {:root root
-                                                                  :wait false})
+                                                                 :wait false})
                     _ (network/wait-for-port "localhost" port)]
                 {:root root
                  :port port
@@ -73,9 +44,9 @@
   []
   (when @*current*
     (atom/swap-return! *current*
-                       (fn [{:keys [process] :as m}]
-                         (os/sh-close process)
-                         [m nil]))))
+      (fn [{:keys [process] :as m}]
+        (os/sh-close process)
+        [m nil]))))
 
 (defn play-file
   "gets the file path in playground"
@@ -96,7 +67,7 @@
   (cond as-script
         (l/emit-script (cons 'do forms) {:lang :js
                                          :layout (or layout :flat)})
-
+        
         :else
         (let [script (l/emit-script (cons 'do forms) {:lang :js
                                                       :layout (or layout :flat)})
@@ -106,20 +77,6 @@
               _ (when (not (fs/exists? filename))
                   (spit filename script))]
           (str sha ".js"))))
-
-(defn play-page
-  "gets a page asset in the playground"
-  {:added "4.0"}
-  [m & [as-script]]
-  (let [page (page-html m)]
-    (if as-script
-      page
-      (let [sha (security/sha1 page)
-            {:keys [root]} (start-playground)
-            name (or (:name m) "page")
-            filename (str name "-" sha ".html")
-            _ (spit (str root "/" filename) page)]
-        filename))))
 
 (defn play-worker
   "constructs the play worker"
