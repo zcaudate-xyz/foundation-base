4710baa85ca0de546d07a7d88ea57ae9ee2c4352
Chris Zheng <chris@statstrade.io>
2026-06-09 22:50:10 +1000

    adding http-websocket

---
+++ src-lang/xt/net/http_websocket.clj
@@ -0,0 +1,51 @@
+(ns xt.net.http-websocket
+  (:require [hara.lang :as l]))
+
+(l/script :xtalk
+  {:require [[xt.lang.spec-base :as xt]
+             [xt.lang.spec-promise :as promise]
+             [xt.lang.common-protocol :as protocol]
+             [xt.net.http-util :as util]]})
+
+(def.xt IWebsocket
+  ["connect"
+   "disconnect"
+   "send"
+   "add_listeners"])
+
+(defn.xt create-base
+  [type methods defaults]
+  (return
+   (xt/x:obj-assign {"::"   (or type "xt.net.http-websocket")
+                     "defaults" (or defaults {})}
+                    (protocol/proto-spec
+                     [[-/IWebsocket methods]]))))
+
+(defn.xt connect
+  "dispatches request through the wrapped fetch client"
+  {:added "4.1.3"}
+  [client opts]
+  (var connect-fn (xt/x:get-key client "connect"))
+  (return (protocol/ensure-promise
+           (connect-fn client input opts))))
+
+(defn.xt disconnect
+  "dispatches request through the wrapped fetch client"
+  {:added "4.1.3"}
+  [client]
+  (var disconnect-fn (xt/x:get-key client "disconnect"))
+  (return (disconnect-fn client)))
+
+(defn.xt send
+  "dispatches request through the wrapped fetch client"
+  {:added "4.1.3"}
+  [client input]
+  (var send-fn (xt/x:get-key client "send"))
+  (return (send-fn client input)))
+
+(defn.xt add-listeners
+  "dispatches request through the wrapped fetch client"
+  {:added "4.1.3"}
+  [client f]
+  (var add-fn (xt/x:get-key client "add_listeners"))
+  (return (add-fn client f)))
