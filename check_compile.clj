bbbaa19fd7376e8c103e9de286902f4b175a923e
Chris Zheng <chris@statstrade.io>
2026-06-14 21:40:03 +1000

    added supabase config

--- check_compile.clj
+++
@@ -1,6 +0,0 @@
-(ns check-compile
-  (:require [hara.runtime.postgres.grammar.common :as common]
-            [hara.runtime.postgres.grammar.form-defpartition :as form-defpartition]))
-
-(println "Common pg-deftype-ref-name:" (resolve 'common/pg-deftype-ref-name))
-(println "Form-defpartition pg-deftype-partition:" (resolve 'form-defpartition/pg-deftype-partition))
