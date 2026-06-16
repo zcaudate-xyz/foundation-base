1d377a02e133940065aaae1be7066a668ac9cf93
Chris Zheng <zcaudate@outlook.com>
2026-06-16 15:28:44 +1000

    changing common-protocol. lcoking in

--- src-lang/xt/lang/common_protocol.clj
+++ src-lang/xt/lang/common_protocol.clj
@@ -11,50 +11,6 @@
 
 (def.xt IMPLEMENTATIONS {})
 
-(defn.xt iface-combine
-  "combines interface vectors without duplicates"
-  {:added "4.1"}
-  [interfaces]
-  (var seen {})
-  (var out [])
-  (xt/for:array [iface interfaces]
-    (xt/for:array [key iface]
-      (when (xt/x:nil? (xt/x:get-key seen key))
-        (xt/x:set-key seen key true)
-        (xt/x:arr-push out key))))
-  (return out))
-
-(defn.xt proto-group
-  "pairs the combined protocol surface with the implementation map"
-  {:added "4.1"}
-  [interfaces spec-map]
-  (return [(-/iface-combine interfaces) spec-map]))
-
-(defn.xt proto-spec
-  "merges protocol groups and rejects missing required methods"
-  {:added "4.1"}
-  [spec-arr]
-  (var acc {})
-  (xt/for:array [entry spec-arr]
-    (var required (xt/x:first entry))
-    (var spec-map (xt/x:second entry))
-    (xt/for:array [key required]
-      (when (xt/x:nil? (xt/x:get-key spec-map key))
-        (throw (xt/x:ex "Invalid Key"
-                        {:required key
-                         :actual (xtd/obj-keys spec-map)}))))
-    (:= acc (xt/x:obj-assign acc spec-map)))
-  (return acc))
-
-(defn.xt ensure-promise
-  "wraps raw values in a promise and preserves native promises"
-  {:added "4.1"}
-  [value]
-  (if (promise/x:promise-native? value)
-    (return value)
-    (return (promise/x:promise-run value))))
-
-
 ;;
 ;; design of protocol and class
 ;;
