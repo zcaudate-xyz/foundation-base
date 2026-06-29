72b227097b0884ecc08071f83a07ff411d248d77
Chris Zheng <zcaudate@outlook.com>
2026-06-29 19:33:11 +1000

    Revert gimp image tag to latest (entrypoint now pins the source tag)

--- src/hara/runtime/gimp/impl.clj
+++ src/hara/runtime/gimp/impl.clj
@@ -297,7 +297,7 @@
     :python :gimp
     {:type :hara/rt.gimp
      :config {:layout :full
-              :container {:image "foundation-base/rt-basic-gimp:debian-buster-py2"
+              :container {:image "foundation-base/rt-basic-gimp:latest"
                           :exec  ["xvfb-run" "-a" "gimp" "-i"
                                   "--batch-interpreter" "python-fu-eval" "-b"]}}
      :instance {:create gimp:create}})])
