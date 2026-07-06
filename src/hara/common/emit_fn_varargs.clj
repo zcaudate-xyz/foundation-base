28698f3c2ab866034212cbcb800ca08c1237db71
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-07-07 03:45:20 +1000

    Implement target-agnostic varargs hook dispatch

--- src/hara/common/emit_fn_varargs.clj
+++ src/hara/common/emit_fn_varargs.clj
@@ -1,1 +1,36 @@
-(ns hara.common.emit-fn-varargs)
+(ns hara.common.emit-fn-varargs
+  (:require [hara.common.emit-helper :as helper]
+            [std.lib.foundation :as f]))
+
+(defn- hook-namespaces
+  [lang]
+  [(symbol (str "hara.model.spec-" (name lang) "-varargs"))
+   (symbol (str "hara.model.spec-" (name lang) ".rest"))])
+
+(defn resolve-hook
+  [mopts hook]
+  (when-let [lang (:lang mopts)]
+    (some (fn [ns-sym]
+            (try
+              (requiring-resolve (symbol (str ns-sym) (name hook)))
+              (catch java.io.FileNotFoundException _
+                nil)))
+          (hook-namespaces lang))))
+
+(defn emit-input
+  [arg grammar mopts]
+  (let [emit-rest (or (get-in grammar [:default :function :args :rest])
+                      (resolve-hook mopts 'emit-input-rest))]
+    (if emit-rest
+      (emit-rest arg grammar mopts)
+      (f/error "Rest argument emitter not configured"
+               {:lang (:lang mopts)
+                :symbol (:symbol arg)}))))
+
+(defn prepare-body
+  [args body grammar mopts]
+  (if (some helper/rest-arg-symbol args)
+    (if-let [prepare (resolve-hook mopts 'prepare-body)]
+      (prepare args body grammar mopts)
+      body)
+    body))
