976f2ba75789a0cc5f23e0668b082c3662d16005
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-07-07 03:34:52 +1000

    Implement Python rest argument emitter

--- src/hara/model/spec_python/rest.clj
+++ src/hara/model/spec_python/rest.clj
@@ -1,1 +1,6 @@
-(ns hara.model.spec-python.rest)
+(ns hara.model.spec-python.rest
+  (:require [hara.common.emit-common :as common]))
+
+(defn emit-input-rest
+  [{:keys [symbol]} grammar mopts]
+  (str "*" (common/*emit-fn* symbol grammar mopts)))
