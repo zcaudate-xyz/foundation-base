6c75fc85c488e27b04e3fe8cde68794dffd1396b
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-07-07 03:43:51 +1000

    Implement Python varargs hooks

--- src/hara/model/spec_python_varargs.clj
+++ src/hara/model/spec_python_varargs.clj
@@ -1,1 +1,10 @@
-(ns hara.model.spec-python-varargs)
+(ns hara.model.spec-python-varargs
+  (:require [hara.common.emit-common :as common]))
+
+(defn emit-input-rest
+  [{:keys [symbol]} grammar mopts]
+  (str "*" (common/*emit-fn* symbol grammar mopts)))
+
+(defn prepare-body
+  [_ body _ _]
+  body)
