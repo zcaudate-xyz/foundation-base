aafd53b3aa90d0d05cb28281e2e8509d1f64834d
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-07-07 03:35:45 +1000

    Implement JavaScript rest argument emitter

--- src/hara/model/spec_js/rest.clj
+++ src/hara/model/spec_js/rest.clj
@@ -1,1 +1,6 @@
-(ns hara.model.spec-js.rest)
+(ns hara.model.spec-js.rest
+  (:require [hara.common.emit-common :as common]))
+
+(defn emit-input-rest
+  [{:keys [symbol]} grammar mopts]
+  (str "..." (common/*emit-fn* symbol grammar mopts)))
