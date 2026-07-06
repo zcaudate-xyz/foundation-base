fbed3f562f9604e3118504277be37418e01d25b5
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-07-07 03:36:03 +1000

    Implement Ruby rest argument emitter

--- src/hara/model/spec_ruby/rest.clj
+++ src/hara/model/spec_ruby/rest.clj
@@ -1,1 +1,6 @@
-(ns hara.model.spec-ruby.rest)
+(ns hara.model.spec-ruby.rest
+  (:require [hara.common.emit-common :as common]))
+
+(defn emit-input-rest
+  [{:keys [symbol]} grammar mopts]
+  (str "*" (common/*emit-fn* symbol grammar mopts)))
