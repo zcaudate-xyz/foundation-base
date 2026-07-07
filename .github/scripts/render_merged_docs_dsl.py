2d9b74fbae13928ab65a2cd1ea87170ba234c320
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-07-07 11:28:43 +1000

    Add code.doc DSL renderer

---
+++ .github/scripts/render_merged_docs_dsl.py
@@ -0,0 +1,5 @@
+from convert_merged_docs_to_dsl import quote, slug, source_title, tokenize
+
+
+def heading_form(kind, title, link):
+    return f'[[:{kind} {{:title {quote(title)} :link {quote(link)}}}]]'
