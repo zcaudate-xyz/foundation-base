cc915616ac8f47fa5428e5ad8a1e0eacbcf87456
Chris Zheng <zcaudate@users.noreply.github.com>
2026-07-19 07:51:02 +1000

    fix xtbench widget module specifier

--- src-doc/documentation/xt/xtbench_metrics.clj
+++ src-doc/documentation/xt/xtbench_metrics.clj
@@ -5,7 +5,7 @@
          :lead "Results are published by the XTBench GitHub Actions workflows and loaded directly from their metrics branch."}]]
 
 [[:widget/js
-  {:src "js/widgets/xtbench.js"
+  {:src "./js/widgets/xtbench.js"
    :class "xtbench-dashboard"
    :props {:data-url "https://raw.githubusercontent.com/zcaudate-xyz/foundation-base/xtbench-metrics/index.json"
            :limit 100}}]]
