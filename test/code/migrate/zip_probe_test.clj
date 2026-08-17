b148baeb598d7569d5fea94ccc04f06559a54041
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-08-17 12:27:22 +1000

    migrate: emit std.lib.zip migration evidence

--- test/code/migrate/zip_probe_test.clj
+++ test/code/migrate/zip_probe_test.clj
@@ -8,6 +8,13 @@
   (.encodeToString (Base64/getEncoder)
                    (.getBytes (str value) "UTF-8")))
 
+(defn emit-base64
+  [label value]
+  (println (str label "_BASE64_BEGIN"))
+  (doseq [chunk (partition-all 4000 (encode-output value))]
+    (println (apply str chunk)))
+  (println (str label "_BASE64_END")))
+
 ^{:refer code.migrate/migrate-pair
   :id std-lib-zip-migration-probe
   :added "4.1"}
@@ -21,12 +28,15 @@
                    :target/path "lib/test/std/lib/zip_test.hal"
                    :source/string (slurp "test/std/lib/zip_test.clj")}
         pair (migrate/migrate-pair source-unit test-unit)]
-    (println "ZIP_MIGRATION_SOURCE_BASE64_BEGIN")
-    (println (encode-output (get-in pair [:source :output])))
-    (println "ZIP_MIGRATION_SOURCE_BASE64_END")
-    (println "ZIP_MIGRATION_TEST_BASE64_BEGIN")
-    (println (encode-output (get-in pair [:test :output])))
-    (println "ZIP_MIGRATION_TEST_BASE64_END")
+    (emit-base64 "ZIP_MIGRATION_SOURCE" (get-in pair [:source :output]))
+    (emit-base64 "ZIP_MIGRATION_TEST" (get-in pair [:test :output]))
+    (emit-base64 "MIGRATION_ENGINE" (slurp "src/code/migrate/engine.clj"))
+    (emit-base64 "MIGRATION_ENGINE_TEST" (slurp "test/code/migrate/engine_test.clj"))
+    (emit-base64 "MIGRATION_SOURCE_RULES" (slurp "resources/code/migrate/rules/source.edn"))
+    (emit-base64 "MIGRATION_TEST_ENGINE" (slurp "src/code/migrate/test.clj"))
+    (emit-base64 "MIGRATION_STAGE" (slurp "src/code/migrate/stage.clj"))
+    (emit-base64 "MIGRATION_PROBE" (slurp "src/code/migrate/probe.clj"))
+    (emit-base64 "MIGRATION_VERIFY" (slurp "src/code/migrate/verify.clj"))
     (println "ZIP_MIGRATION_EVIDENCE_BEGIN")
     (prn (update-vals pair #(dissoc % :input :output)))
     (println "ZIP_MIGRATION_EVIDENCE_END")
