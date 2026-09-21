34c7e03554e22273f86eca36af6806e91d8ad8af
Chris Zheng <zcaudate@users.noreply.github.com>
2026-09-21 10:42:12 +1000

    adding more deploy options

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xyz.zcaudate/foundation-base "4.1.6"
+(defproject xyz.zcaudate/foundation-base "4.1.8"
   :description "base libraries for foundation"
   :url "https://www.github.com/zcaudate-xyz/foundation-base"
   :license  {:name "MIT License"
@@ -16,10 +16,10 @@
    "doc-check"   ["exec" "-ep" "(require '[code.doc.check :as doc-check]) (System/exit (min 1 (doc-check/check-failures :all)))"]
    "codox"       ["with-profile" "+codox" "run" "-m" "code.doc.codox"]
    "install"     ["exec" "-ep" "(use 'code.tool.maven)   (install :all {:tag :all}) (System/exit 0)"]
-   "deploy"      ["exec" "-ep" "(use 'code.tool.maven)   (deploy :all {:tag :all}) (System/exit 0)"]
+   "deploy-github" ["exec" "-ep" "(use 'code.tool.maven)   (deploy :all {:tag :all}) (System/exit 0)"]
    "package-clojars" ["exec" "-ep" "(use 'code.tool.maven)   (let [result (package :all {:tag :clojars})] (System/exit (task-exit-code result)))"]
    "deploy-clojars" ["exec" "-ep" "(use 'code.tool.maven)   (let [result (deploy :all {:tag :clojars})] (System/exit (task-exit-code result)))"]
-   "deploy-root" ["exec" "-ep" "(require '[leiningen.core.project :as project] '[leiningen.deploy :as deploy]) (deploy/deploy (project/read \"project.clj\") \"clojars\") (System/exit 0)"]
+   "deploy-root" ["deploy" "clojars"]
    "deploy-lein" ["exec" "-ep" "(use 'code.tool.maven)   (deploy-lein :all {:tag :all}) (System/exit 0)"]
    "push-native-code"  ["run" "-m" "component.task-native-index"]
    "push-c-000-pthreads"        ["run" "-m" "play.c-000-pthreads-hello.build"]
